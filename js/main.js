683ee10325755a368db66f1917a0dded64e718db
Luis Blanco <74207671+LuisBlanco0104@users.noreply.github.com>
2021-10-25 21:18:55 +0200

    main.js updated to change the 'tab-content' and 'tab-item' on click buttoms

--- js/main.js
+++ js/main.js
@@ -4,12 +4,18 @@
     tabsContainer = document.querySelector(".about-tabs");
 
         tabsContainer.addEventListener("click", (event) =>{
-            /* if event.target contains 'tab-item' class and not contains 'active' */
+            /* if event.target contains "tab-item" class and not contains "active" */
             if(event.target.classList.contains("tab-item") &&
             !event.target.classList.contains("active")){
-                console.log("event.target contains 'tab-item' class and not contains 'active' class");
-                console.log(event.target);
+                const target = event.target.getAttribute("data-target");
+                console.log(target);
+                /* deactivate existing active "tab-item" */
+                tabsContainer.querySelector(".active").classList.remove("outer-shadow","active");
+                event.target.classList.add("outer-shadow", "active");
+                /* deactivate existing active "tab-content" */
+                aboutSection.querySelector(".tab-content.active").classList.remove("active");
+                // active new 'tab-content' //
+                aboutSection.querySelector(target).classList.add("active");
             }
         })
-
-})();
+})(); 
